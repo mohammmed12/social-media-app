e0900674fa57c2c563028cc3292fc88c7d67011c
unknown <mostafasaad55@gmail.com>
2020-05-19 09:56:59 +0200

    added mongoose connection

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-app.use(morgan("dev"));
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//db connect
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("db connected"));
+
+mongoose.connection.on("error", (err) =>
+  console.log(`db error ${err.message}`)
+);
 
 const postRoutes = require("./routes/posts");
 
+// middleware
+app.use(morgan("dev"));
 app.use("/", postRoutes);
 
-port = 8000;
+port = process.env.PORT || 8000;
 app.listen(port, () => console.log("server started"));
